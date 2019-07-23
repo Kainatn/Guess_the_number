9c5bcabcb28e69dc46ed400c5ac4607d8a4ccd72
Kainat Naz <49521321+Kainatn@users.noreply.github.com>
2019-07-23 15:50:16 +0500

    Add files via upload

---
+++ medium/script.js
@@ -0,0 +1,20 @@
+let count = 0;
+let numb = 500;
+let randm = Math.floor(Math.random() * numb);
+
+function guessNumber() {
+
+    let guessedNumber = document.querySelector('input').value;
+    count++;
+    if (guessedNumber != '') {
+        if (guessedNumber == randm) {
+            alert('Congratulation You guessed the ' + randm + " in " + count + "tries");
+        } else if (guessedNumber < randm) {
+            alert("You guessed Small Number");
+        } else if (guessedNumber > randm) {
+            alert("You guessed Big Number");
+        }
+    } else {
+        alert("Enter number First");
+    }
+}
